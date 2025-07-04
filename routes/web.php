84604c63748fc5b8ba3482e8dc61b5402ccf3cdc
ridhosamir <ridhosamirabbani1209@gmail.com>
2025-07-05 00:44:04 +0700

    Membuat Tampilan dan CRUD Portal Utama

--- routes/web.php
+++ routes/web.php
@@ -23,11 +23,6 @@ Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-// // Admin Routes - Pastikan route ini terdaftar dengan benar
-// Route::get('/admin/dashboard', function () {
-//     return view('admins.dashboard-admin');
-// })->name('admins.dashboard')->middleware('auth');
-
 // Alternatif cara mendefinisikan route
 Route::middleware(['auth:admin'])->group(function () {
     Route::get('/admin/dashboard', function () {
@@ -38,10 +33,6 @@ Route::middleware(['auth:admin'])->group(function () {
         return view('admins.portal-admin');
     })->name('admins.portal-admin');
 
-    Route::get('/admin/portal-utama', function () {
-        return view('admins.portal-utama');
-    })->name('admins.portal-utama');
-
     Route::get('/admin/manage-reminder', function () {
         return view('admins.manage-reminder');
     })->name('admins.manage-reminder');
@@ -50,8 +41,16 @@ Route::middleware(['auth:admin'])->group(function () {
         return view('admins.manage-poster');
     })->name('admins.manage-poster');
 
-    Route::resource('reminder', ReminderController::class);
     Route::resource('portal-admin', PortalAdminController::class);
-    Route::resource('portal-utama', PortalUtamaController::class);
+    Route::resource('portal-utama', PortalUtamaController::class)->names([
+        'index' => 'admins.portal-utama',
+        'create' => 'admins.portal-utama.create',
+        'store' => 'admins.portal-utama.store',
+        'show' => 'admins.portal-utama.show',
+        'edit' => 'admins.portal-utama.edit',
+        'update' => 'admins.portal-utama.update',
+        'destroy' => 'admins.portal-utama.destroy'
+    ]);
+    Route::resource('reminder', ReminderController::class);
     Route::resource('poster', PosterController::class);
 });
